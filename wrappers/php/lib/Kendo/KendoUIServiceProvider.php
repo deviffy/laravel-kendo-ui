bf2eb28b62bf6171ee51c5a894258a200b194f53
Catalin Dumitrescu <onecdumitrescu@gmail.com>
2017-05-05 05:52:18 +0300

    fix publish path

--- wrappers/php/lib/Kendo/KendoUIServiceProvider.php
+++ wrappers/php/lib/Kendo/KendoUIServiceProvider.php
@@ -7,14 +7,14 @@ use Illuminate\Support\ServiceProvider;
 class KendoUIServiceProvider extends ServiceProvider {
 
     /**
-     * Kendo assets.
+     * Bootstrap the application events.
      *
      * @return void
      */
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../../content/' => public_path('vendor/deviffy'),
+            __DIR__.'/../../content/' => public_path('vendor/kendo'),
         ], 'public');
     }
 
@@ -28,6 +28,16 @@ class KendoUIServiceProvider extends ServiceProvider {
         $this->app->singleton('laravel-kendo-ui', function ($app) {
             return new DataSourceManager($app);
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laravel-kendo-ui'];
     }
 
 }
